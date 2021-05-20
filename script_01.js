d68febdce76516d822e8dbb4d23041ec9d4d25dd
Marco <marco_2@gmx.de>
2021-05-20 11:01:47 +0200

    Funktion + 1 Parameter

--- script_01.js
+++ script_01.js
@@ -22,5 +22,24 @@ function ausgabeNamen() {
     console.log("Hallo " + firstName + "!"); // refactoring == Umformung von Code ohne dass sich die Darstellung im Ergebnis ändert
 }
 
-ausgabeNamen()
-console.log(firstName); // Fehler --> weil lokale(!!!) Variable
+//ausgabeNamen()
+//console.log(firstName); // Fehler --> weil lokale(!!!) Variable
+
+/***** Funktionen 02b *****/
+// 2a. Parametrisierung + Datenübergabe von AUSSEN
+
+function ausgabeNamenParam(firstName) {
+
+    // wenn firstName leer, dann "nobody"
+    if (firstName == undefined || firstName == "" || !firstName) {              // je nach Anforderung kann es sinnvoller sein, !firstName anstelle von firstName == undefined zu verwenden
+        firstName = "Terence Hill"
+    }
+    
+    console.log("Hallo " + firstName + "!"); // refactoring == Umformung von Code ohne dass sich die Darstellung im Ergebnis ändert
+}
+
+ausgabeNamenParam("Spencer"); //Call + Argument(e)
+ausgabeNamenParam("Quentin");
+ausgabeNamenParam("Butch");
+ausgabeNamenParam();
+ausgabeNamenParam(prompt("wie heißt Du?"));
