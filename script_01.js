32180e08766790f010c406d4eb5a5440ee0cde32
Marco <marco_2@gmx.de>
2021-05-20 10:15:07 +0200

    Funktionen

--- script_01.js
+++ script_01.js
@@ -2,3 +2,25 @@
 /***** Funktionen 01 *****/
 
 // 1. Kapselung von Codeblöcken
+
+// Funktionsrumpf (body)
+function test()
+{
+    console.log("Hallo Marco!");
+}
+
+// Codeblock wird eingeleitet durch "{" und beendet durch "}"
+
+// Funktionsaufruf (call)
+//test();
+
+/***** Funktionen 02a *****/
+// 2a. Parametrisierung + Datenübergabe von INNEN
+
+function ausgabeNamen() {
+    let firstName = "Spencer"; // Variable --> lokal (nur innerhalb des Codeblocks!)
+    console.log("Hallo " + firstName + "!"); // refactoring == Umformung von Code ohne dass sich die Darstellung im Ergebnis ändert
+}
+
+ausgabeNamen()
+console.log(firstName); // Fehler --> weil lokale(!!!) Variable
